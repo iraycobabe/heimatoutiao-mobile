0f154487cf923c5db4536768f6b8b9978d09d52a
“hejiawei” <469994761@qq.com>
2023-02-07 11:09:04 +0800

    项目初始化-调整初始目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,11 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //在rules中添加自定义规则
+    "vue/multi-word-component-names":"off",//关闭组件命名规则
+    "vue/no-unused-components": "off", // 当存在定义而未使用的组件时，关闭报错
+    "no-unused-vars":"off", // 当存在定义而未使用的变量时，关闭报错
+    "no-trailing-spaces":"off",//这禁止掉 行尾空格
   }
 }
